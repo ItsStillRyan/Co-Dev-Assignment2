081959bc0544179a64b5da416f60bdad2cbfbccd
ItsStillRyan <75793260+ItsStillRyan@users.noreply.github.com>
2023-02-14 22:35:13 +0800

    first commit, initalized technologies to be used in back and front end

---
+++ Back-End/src/main/kotlin/com/ryandev/codevassignment2/Codevassignment2Application.kt
@@ -0,0 +1,11 @@
+package com.ryandev.codevassignment2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Codevassignment2Application
+
+fun main(args: Array<String>) {
+	runApplication<Codevassignment2Application>(*args)
+}
